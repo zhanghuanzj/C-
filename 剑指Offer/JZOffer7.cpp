b2e854457c59eea86738e7ebdf4a1a237430c22a
zhanghuanzj <840796340@qq.com>
2016-05-25 11:01:02 +0800

    用两个栈实现队列

---
+++ 剑指Offer/JZOffer7.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+    void push(int node) {
+        stack1.push(node);
+    }
+
+    int pop() {
+        if (stack2.empty())
+        {
+        	while(!stack1.empty())
+        	{
+        		stack2.push(stack1.top());
+        		stack1.pop();
+        	}
+        }
+        int result =0;
+        if (!stack2.empty())
+        {
+        	result = stack2.top();
+        	stack2.pop();
+        }
+        return result;
+    }
+
+private:
+    stack<int> stack1;
+    stack<int> stack2;
+};
