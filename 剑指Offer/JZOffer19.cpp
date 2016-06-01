f72889c0224bf897fe29e98586cb1f5237607681
zhanghuanzj <840796340@qq.com>
2016-06-01 11:10:54 +0800

    树的镜像

---
+++ 剑指Offer/JZOffer19.cpp
@@ -0,0 +1,24 @@
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};*/
+class Solution {
+public:
+    void Mirror(TreeNode *pRoot) {
+    	if (pRoot==NULL)
+    	{
+    		return;
+    	}
+    	else
+    	{
+    		swap(pRoot->left,pRoot->right);
+    		Mirror(pRoot->left);
+    		Mirror(pRoot->right);
+    	}
+    }
+};
