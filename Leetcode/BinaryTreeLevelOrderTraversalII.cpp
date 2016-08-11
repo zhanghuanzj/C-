be6017ca822582e42eb2061b5fb3d0c4ce50bef5
zhanghuanzj <840796340@qq.com>
2016-08-11 11:02:33 +0800

    层次遍历逆序

---
+++ Leetcode/BinaryTreeLevelOrderTraversalII.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int> > levelOrderBottom(TreeNode *root) {
+        vector<vector<int>> result;
+        if(root==nullptr) return result;
+        queue<TreeNode*> record;
+        record.push(root);
+        int remain = 1,newRemain = 0;
+        vector<int> level;
+        while(!record.empty()){
+            TreeNode *node = record.front();
+            record.pop();
+            if(node->left){
+                record.push(node->left);
+                ++newRemain;
+            }
+            if(node->right){
+                record.push(node->right);
+                ++newRemain;
+            }
+            --remain;
+            level.push_back(node->val);
+            if(!remain){
+                remain = newRemain;
+                newRemain = 0;
+                result.push_back(level);
+                level.clear();
+            }
+        }
+        reverse(result.begin(),result.end());
+        return result;
+    }
+};
