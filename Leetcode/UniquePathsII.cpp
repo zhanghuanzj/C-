ba1c87a2b61fa083cc0f08669b7bf5d857fc3031
zhanghuanzj <840796340@qq.com>
2016-08-17 14:54:50 +0800

    动态规划

---
+++ Leetcode/UniquePathsII.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
+        if(obstacleGrid.empty()) return 0;
+        int m = obstacleGrid.size();
+        int n = obstacleGrid.front().size();
+        vector<int> dp(n,0);
+        dp[0] = 1;
+        for(int i=0;i<m;++i){
+            for(int j=0;j<n;++j){
+                if(obstacleGrid[i][j]==1){
+                    dp[j] = 0;
+                }else if(j>0){
+                    dp[j] += dp[j-1];
+                }
+            }
+        }
+        return dp[n-1];
+    }
+};
